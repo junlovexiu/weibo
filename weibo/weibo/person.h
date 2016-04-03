a0c65f79b73393f515d5f9d7d452aea50fc66c8f
hujun <hujun@gmail.com>
2016-04-04 00:09:34 +0800

    123123

---
+++ weibo/weibo/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  weibo
+//
+//  Created by HuJun on 16/4/4.
+//  Copyright © 2016年 胡军. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ weibo/weibo/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  weibo
+//
+//  Created by HuJun on 16/4/4.
+//  Copyright © 2016年 胡军. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
