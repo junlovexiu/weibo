abb9ba88d89adfe046b0252165eb239ad941ed00
hujun <hujun@gmail.com>
2016-04-04 00:07:49 +0800

    123123

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by HuJun on 16/4/4.
+//  Copyright © 2016年 胡军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weibo/weibo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weibo
+//
+//  Created by HuJun on 16/4/4.
+//  Copyright © 2016年 胡军. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
